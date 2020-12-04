fbb4dabb751db95146c8e32703bd4c27669c6fe9
Dustin Schuemann <dschuemann@gmail.com>
2020-12-04 14:51:27 -0500

    Testing ECS

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing GITHUB Actions.
+          Testing ECS.
         </p>
         <a
           className="App-link"
