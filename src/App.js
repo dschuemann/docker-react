b51769f72310790a64e5d00d4a3b001450660604
Dustin Schuemann <dschuemann@gmail.com>
2020-12-04 16:27:34 -0500

    Here we go.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing ECS.
+          Testing ECS Again.
         </p>
         <a
           className="App-link"
