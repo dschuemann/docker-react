b68261368170f66ba00994a756faedf27307dbd6
Dustin Schuemann <dschuemann@gmail.com>
2020-12-04 13:30:35 -0500

    Testing EB deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing a pull request.
+          Testing GITHUB Actions.
         </p>
         <a
           className="App-link"
